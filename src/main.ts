0fb0107ebb158d1b22b1353b8616f294f79ee406
raydevcp20 <rayanecristina.19@gmail.com>
2024-03-20 19:52:33 -0300

    fix: alterações pra suportar o roteamento do angular 17

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
