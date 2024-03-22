70eab2be0e39faf9f549155bbaea7ca7dea30b57
raydevcp20 <rayanecristina.19@gmail.com>
2024-03-22 17:48:11 -0300

    feat: organizando o projeto por conteudo e exemplos

--- src/app/alimentos/create/create.component.ts
+++ src/app/alimentos/create/create.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  alimentoForm: any = {};
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.alimentoForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      calorias: ['', Validators.required],
+      proteinas: ['', Validators.required],
+      potassio: ['', Validators.required],
+      colesterol: ['', Validators.required],
+      gordura: ['', Validators.required],
+      imagem: ['', Validators.required]
+    });
+  }
+
+  submitForm(): void {
+    if (this.alimentoForm.valid) {
+      // Implementar a lógica para salvar os dados
+      console.log(this.alimentoForm.value);
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ToUppercasePipe } from './pipes/to-uppercase.pipe';
-import { ConversorPipe } from './pipes/conversor.pipe';
-import { TesterDirective } from './tester.directive';
+import { ToUppercasePipe } from './conteudos/pipes/to-uppercase.pipe';
+import { ConversorPipe } from './conteudos/pipes/conversor.pipe';
+import { TesterDirective } from './conteudos/tester.directive';
 import { HomeComponent } from './home/home.component';
-import { CalculadoraService } from './services/calculadora.service';
+import { CalculadoraService } from './conteudos/services/calculadora.service';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
